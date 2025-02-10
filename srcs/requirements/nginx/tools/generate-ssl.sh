00b25be0f4d04253efe426d71dcbde698484b29b
LeaYeh <smile2140@gmail.com>
2025-02-10 21:43:33 +0100

    feat: Add more config for security issues

--- srcs/requirements/nginx/tools/generate-ssl.sh
+++ srcs/requirements/nginx/tools/generate-ssl.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
+if [ -z "$DOMAIN_NAME" ]; then
+    echo "Error: DOMAIN_NAME environment variable is not set"
+    exit 1
+fi
 
-openssl genpkey -algorithm RSA -out /etc/nginx/ssl/privatekey.pem
-
-openssl req -new -key /etc/nginx/ssl/privatekey.pem -out /etc/nginx/ssl/csr.pem -subj "/C=FR/ST=Paris/L=Paris/O=42/OU=42/CN=ft_services"
-
-openssl x509 -req -days 365 -in /etc/nginx/ssl/csr.pem -signkey /etc/nginx/ssl/privatekey.pem -out /etc/nginx/ssl/certificate.pem
-
-rm /etc/nginx/ssl/csr.pem
+openssl req -x509 -nodes \
+    -days 365 \
+    -newkey rsa:2048 \
+    -keyout /etc/nginx/ssl/privatekey.pem \
+    -out /etc/nginx/ssl/certificate.pem \
+    -subj "/C=FR/ST=IDF/L=Paris/O=42/OU=42/CN=${DOMAIN_NAME}" \
+    -addext "subjectAltName=DNS:${DOMAIN_NAME}"
 
 chmod 600 /etc/nginx/ssl/privatekey.pem
-chmod 600 /etc/nginx/ssl/certificate.pem
-
+chmod 644 /etc/nginx/ssl/certificate.pem
